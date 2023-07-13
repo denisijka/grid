17a6edc84333c32d67d459d93c0cd1df543ec1d8
denisijka <37539630+denisijka@users.noreply.github.com>
2023-07-13 21:30:36 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<style>
+  .domn-grid-container{
+  width: 100%;
+  max-width: 100%;
+  display: grid;
+  grid-auto-flow: row dense;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+}
+.domn-grid-container a{
+  color: #0F1511;
+  padding: 10px 17px;
+  font-size: 16px;
+  line-height: 1;
+  text-align: center;
+}
+</style>
+         
+  <div class="domn-grid-container">
+    <a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a>
+    <a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a>
+    <a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a><a href="#">d1ce78</a>
+  </div>
+       
